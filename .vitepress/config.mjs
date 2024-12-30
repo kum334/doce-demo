8657863160b0dc47be406681258d35c511ef0854
DESKTOP-QVGCD5I\kun <200255>
2024-12-30 20:39:46 +0800

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,86 @@
+import { defineConfig } from 'vitepress'
+import { set_sidebar } from "./utils/auto-sidebar.mjs";	// 改成自己的路径
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  head: [["link", { rel: "icon", href: "/logo.png" }]],
+  title: "我的项目",
+  description: "A VitePress Site",
+  themeConfig: {
+
+
+       // 设置搜索框的样式
+       search: {
+        provider: "local",
+        options: {
+          translations: {
+            button: {
+              buttonText: "搜索文档",
+              buttonAriaLabel: "搜索文档",
+            },
+            modal: {
+              noResultsText: "无法找到相关结果",
+              resetButtonTitle: "清除查询条件",
+              footer: {
+                selectText: "选择",
+                navigateText: "切换",
+              },
+            },
+          },
+        },
+      },
+    logo:"/logo.png",
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '家',
+        items:[
+        {text:'首页',link:'/'},
+        {text:'markdown',link:'/markdown-examples'},
+        {text:'黄库',link:'/markdown-examples'}
+      ] , 
+    },
+      
+      {text: "mark示例", link : "/markdown-examples"  },
+      {text:"自动生成1", link : "/front-end/react/"  },
+      {text:"自动生成2", link : "/backend/rabbitmq/" },
+   
+    ],
+
+    // sidebar: [
+    //   {
+    //     text: 'Examples',
+    //     items: [
+    //       { text: 'Markdown 演示', link: '/markdown-examples' },
+    //       { text: 'Runtime API 演示', link: '/api-examples' }
+    //     ]
+        
+    //   }, {
+    //     text: 'Examples',
+    //     items: [
+    //       { text: 'Markdown 演示', link: '/markdown-examples' },
+    //       { text: 'Runtime API 演示', link: '/api-examples' }
+    //     ]
+        
+    //   }
+    // ],
+
+    sidebar: {
+     
+       "/front-end/react": set_sidebar("front-end/react"),
+       "/backend/rabbitmq": set_sidebar("backend/rabbitmq"),
+     },
+     
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/kum334' }
+    ]
+    ,
+
+
+    footer: 
+      { copyright:"Copyright@ 2024 Albert Huang"}
+    
+  }
+
+  
+})
---
+++ index.md
@@ -0,0 +1,28 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "我的文档项目"
+  text: "一个文档站点"
+  tagline: 持续学习，持续进步
+  image:
+  src: /background.svg
+  alt: 背景图片
+  actions:
+    - theme: brand
+      text: Markdown 示例
+      link: https://www.jd.com
+    - theme: alt
+      text: API 示例
+      link: /api-examples
+
+features:
+  - title: 特性一 🎇
+    details: 特征1的详细描述
+  - title: Feature B
+    details: 特征2的详细描述
+  - title: Feature C
+    details: 特征3的详细描述
+---
+
